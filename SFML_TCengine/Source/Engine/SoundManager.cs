15fd0216d10951df733ecfe87a72b2d937143be7
Joloharo <joeljoanlopez@gmail.com>
2023-06-15 15:03:30 +0200

    Create SoundManager.cs

---
+++ SFML_TCengine/Source/Engine/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using SFML.Audio;
+using System.Collections.Generic;
+
+namespace TCEngine
+{
+    public class SoundManager
+    {
+        private List<Sound> m_Sounds = new List<Sound>();
+
+        public void Update()
+        {
+            var soundsToRemove = m_Sounds.FindAll(x => x.Status == SoundStatus.Stopped);
+            soundsToRemove.ForEach(x => x.Dispose());
+            m_Sounds.RemoveAll(soundsToRemove.Contains);
+        }
+
+        public void PlaySound(string _soundName, float _volume = 100.0f)
+        {
+            SoundBuffer buffer = new SoundBuffer(_soundName);
+            Sound sound = new Sound(buffer);
+            sound.Volume = _volume;
+            sound.Play();
+            m_Sounds.Add(sound);
+        }
+    }
+}
